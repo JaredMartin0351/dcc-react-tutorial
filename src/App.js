cb1a4de0090126f586624f8f565d70550cf40be8
JaredMartin0351 <jaredmartin0351@gmail.com>
2022-01-06 10:08:53 -0600

    added entry form with submit button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
 
 function App() {
 
   const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}, {weight: 150, date: '12-25-2021'}])
 
+  function addNewEntry(entry){
+    
+    let tempEntries = [...entries, entry];
+    
+    setEntries(tempEntries);
+  }
+
   return (
     <div>
       <DisplayEntries parentEntries = {entries} />
-      
+      <AddEntryForm addNewEntryProperty = {addNewEntry}/>
     </div>
   );
 }
