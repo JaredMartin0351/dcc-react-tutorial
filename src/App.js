efb2225200602f1e4e01aac2e1114536ccef611c
JaredMartin0351 <jaredmartin0351@gmail.com>
2022-01-06 10:57:22 -0600

    added functionality to take in weight and date and update the line chart on the page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 import AddEntryForm from './Components/AddEntry/AddEntryForm';
+import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
 
 function App() {
 
@@ -15,8 +16,9 @@ function App() {
 
   return (
     <div>
-      <DisplayEntries parentEntries = {entries} />
+      <DisplayEntries parentEntries = {entries}/>
       <AddEntryForm addNewEntryProperty = {addNewEntry}/>
+      <EntriesChartTracker parentEntries={entries}/> 
     </div>
   );
 }
